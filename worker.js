9090ab14edd2c61129c0d3e1043e2326267ccb62
bffbtsarmy <140270659+bffbtsarmy@users.noreply.github.com>
2025-05-23 11:44:22 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,13 +1,5 @@
 export default {
   async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-    let path = url.pathname === "/" ? "/index.html" : url.pathname;
-
-    try {
-      const asset = await env.ASSETS.fetch(request);
-      return asset;
-    } catch {
-      return new Response("Not found", { status: 404 });
-    }
+    return env.ASSETS.fetch(request);
   },
 };
