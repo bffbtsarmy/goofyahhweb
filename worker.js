769385397219399a2dc391a0875c5cdc891c90d1
bffbtsarmy <140270659+bffbtsarmy@users.noreply.github.com>
2025-05-23 11:31:26 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,10 +1,12 @@
-import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
-
 export default {
   async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    let path = url.pathname === "/" ? "/index.html" : url.pathname;
+
     try {
-      return await getAssetFromKV({ request });
-    } catch (e) {
+      const asset = await env.ASSETS.fetch(request);
+      return asset;
+    } catch {
       return new Response("Not found", { status: 404 });
     }
   },
